7f0e639d6acfe0242069687bfb6ebdca49eb1b79
Alexander Timoshenko <timo_shenko@msn.com>
2017-06-15 16:18:16 +0200

    Update writeppm.cpp
    float cast

--- HelloMrRay/snippets/writeppm.cpp
+++ HelloMrRay/snippets/writeppm.cpp
@@ -13,8 +13,10 @@ int main() {
 	for (int j = height - 1; j >= 0; j--) {
 		for (int i = 0; i < width; i++) {
 			//RGB
-			float color[3] = {float(i)/ float(width),float(j)/float(height),0.2f };
-			outFile << int(255.99 * color[0]) << " "<< int(255.99 * color[1]) << " " << int(255.99 * color[2]) << "\n";
+			outFile
+				<< int(255.99 * i / width) << " "
+				<< int(255.99 *j / height) << " "
+				<< int(255.99 * 0.2f) << "\n";
 		}
 	}
 }
