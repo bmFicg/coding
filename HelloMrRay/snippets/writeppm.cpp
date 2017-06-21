d9ab71cab25d2a40a257f0b7962b4c5189d66fb9
Alexander Timoshenko <timo_shenko@msn.com>
2017-06-21 03:33:52 +0200

    add
    min g++

--- HelloMrRay/snippets/writeppm.cpp
+++ HelloMrRay/snippets/writeppm.cpp
@@ -18,6 +18,7 @@ int main() {
 	}
 }
 
+
 /* one liner
 #include<fstream>
 int main(int argc,char*argv[]){std::ofstream o((const char*)(std::string(argv[0]).append(".ppm")).c_str());o<<"P3\n640 360\n255\n";for(int j=360;j-->=0;)for(int i=0;i++<640;)o<<255*i/640<<std::endl<<255*j/360<<std::endl<<255*.2f<<std::endl;}
