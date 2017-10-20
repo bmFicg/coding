81f6b99046a6ba9ffe2b0f4762dd1fd3891f6627
Alexander Timoshenko <timo_shenko@msn.com>
2017-10-21 00:28:46 +0200

    Update writeppm.cpp

--- HelloMrRay/snippets/writeppm.cpp
+++ HelloMrRay/snippets/writeppm.cpp
@@ -18,18 +18,6 @@ int main() {
 	}
 }
 
-#include<fstream>
-int main(int, char**argv) {
-	int width = 640, height = 360;
-	std::ofstream o((char*)(std::string(*argv).append(".ppm")).c_str());
-	o << "P3\n" << width << " " << height << "\n255\n";
-	for (float j = height-1.f;j-- >= 0;) {
-		for (float i = 0; i++ < width;)
-			o << int(255.99 * i / width) << " " << int(255.99 * j / height) << " " << int(255.99 *.2) << "\n";
-	}
-	o.flush();
-}
-
 /* one liner
 #include<fstream>
 int main(int, char**argv){std::ofstream o((char*)(std::string(*argv).append(".ppm")).c_str());o<<"P3\n640 360\n255\n";for(int j=360;j-->=0;)for(int i=0;i++<640;)o<<255*i/640<<" "<<255*j/360<<" "<<255*.2f<<"\n";}
